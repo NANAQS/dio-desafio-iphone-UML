d16c00ec23a16c55cf90136194876f26adecd959
NANAQS <otaviotokyo5@gmail.com>
2024-04-28 20:41:25 -0300

    chore: add all files

---
+++ src/IPhone.java
@@ -0,0 +1,53 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void exibirPagina(String url) {
+        throw new UnsupportedOperationException("Unimplemented method 'exibirPagina'");
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarNovaAba'");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        throw new UnsupportedOperationException("Unimplemented method 'atualizarPagina'");
+    }
+
+    @Override
+    public void ligar() {
+        throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+    }
+
+    @Override
+    public void atender() {
+        throw new UnsupportedOperationException("Unimplemented method 'atender'");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        throw new UnsupportedOperationException("Unimplemented method 'iniciarCorreioVoz'");
+    }
+
+    @Override
+    public void fazerChamada(String numero) {
+        throw new UnsupportedOperationException("Unimplemented method 'fazerChamada'");
+    }
+
+    @Override
+    public void tocar() {
+        throw new UnsupportedOperationException("Unimplemented method 'tocar'");
+    }
+
+    @Override
+    public void pausar() {
+        throw new UnsupportedOperationException("Unimplemented method 'pausar'");
+    }
+
+    @Override
+    public void selecionarMusica(String nomeMusica) {
+        throw new UnsupportedOperationException("Unimplemented method 'selecionarMusica'");
+    }
+    
+}
